fe357a11e8bbcd1986c6aa70366c0b4cedb95a21
runnan <79387455+RunnanC-137@users.noreply.github.com>
2022-07-01 19:36:01 -0300

    up

--- Programador_Br/jQuery/index.js
+++ Programador_Br/jQuery/index.js
@@ -1,6 +1,11 @@
 const jq = $.noConflict() 
 const log = console.log
 const query = (element) => document.querySelector(element)
-jq("h1").click( e => log(e))
+
+jq("button").click( (e=true) => {
+      
+    jq("h2").fadeToggle(1500)
+    
+})
 //jq("h1").mousemove( e => col(e.target))
 jq("body").mousemove(e => query("h1").innerHTML = `${e.screenX}, ${e.screenY}`)
