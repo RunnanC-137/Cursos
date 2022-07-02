4eeb0571b7972d5b2c23f808d03e7013657e9970
runnan <79387455+RunnanC-137@users.noreply.github.com>
2022-07-02 16:55:56 -0300

    Update index.js

--- Programador_Br/jQuery/index.js
+++ Programador_Br/jQuery/index.js
@@ -1,11 +1,11 @@
 const jq = $.noConflict() 
 const log = console.log
-const query = (element) => document.querySelector(element)
+
 
 jq("button").click( (e=true) => {
       
-    jq("h2").fadeToggle(1500)
+    jq("h2").fadeToggle(1500).animate(100)
     
 })
 //jq("h1").mousemove( e => col(e.target))
-jq("body").mousemove(e => query("h1").innerHTML = `${e.screenX}, ${e.screenY}`)
+jq("body").mousemove(e => jq("h1").html(`${e.screenX}, ${e.screenY}`))
